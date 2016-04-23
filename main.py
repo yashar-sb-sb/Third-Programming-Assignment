8606daf6851b95d23ce3ed455270ddca364beb94
Yashar Shahi <yashar_sb_sb@yahoo.com>
2016-04-23 23:36:52 +0430

    Add generate function.
    this function yeilds union of every two sets included in it's input.

--- main.py
+++ main.py
@@ -3,4 +3,14 @@ def apriori(data,min_support):
     data should be an iterable containing sets of items.
     (item can be any hashable type.)
     """
-    
+
+    def generate(sets):
+        """sets should contain sets of same length.
+        This iterator generator function yields union of every two sets included in sets. provided that the two sets have exactly one uncommon item.
+        """
+        for i in sets:
+            for j in sets:
+                if i != j:
+                    new_set = i.union(j)
+                    if len(new_set) == len(i)+1:
+                        yield frozenset(new_set)
