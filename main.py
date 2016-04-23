4bdeaee01a5a9b72b3f565e6dcb6f216408cd7c2
Yashar Shahi <yashar_sb_sb@yahoo.com>
2016-04-23 23:53:08 +0430

    Update function apriori
    Add code to compute frequent itemsets with sizes greater than 1.
    Return the result.

--- main.py
+++ main.py
@@ -15,5 +15,14 @@ def apriori(data,min_support):
                     if len(new_set) == len(i)+1:
                         yield frozenset(new_set)
 
-        #generate itemsets of size one.
-        sets = [set().union(*map(lambda x: [frozenset(i)for i in x if sum(i in j for j in data) >= min_support],data))]
+    #generate itemsets of size one.
+    sets = [set().union(*map(lambda x: [frozenset(i)for i in x if sum(i in j for j in data) >= min_support],data))]
+
+    #generate itemsets with sizes greater than 1
+    while True:
+        new_sets = {i for i in generate(sets[-1]) if sum(i.issubset(j) for j in data) >= min_support}
+        if len(new_sets): sets.append(new_sets)
+        else: break
+
+    #returns the resulting itemsets with frequency of at least min_support
+    return sets
